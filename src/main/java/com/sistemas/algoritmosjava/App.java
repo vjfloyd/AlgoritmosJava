72be019682ea01d04a27841cace0ceff0276141c
Victor Rojas <vjrojasb@gmail.com>
2016-02-15 22:17:26 -0500

    proyecto base

---
+++ src/main/java/com/sistemas/algoritmosjava/App.java
@@ -0,0 +1,28 @@
+package com.sistemas.algoritmosjava;
+
+import java.util.List;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    
+    
+    public static void main( String[] args )
+    {
+    //  System.out.println( "Hello World!" );
+       List<Integer> numeros = null;
+        for (int i = 0; i < 10; i++) {
+            numeros.add(i);
+        }
+    
+    
+    }
+    
+    public static void Ordenar(){
+        System.out.print(1);
+    }
+    
+}
